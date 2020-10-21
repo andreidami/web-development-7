2b8abd0ab5fbc777d25a5682939eaad657d2f0b3
Damian Andrei <andrei_damian27@yahoo.com>
2020-10-21 16:53:40 +0300

    add changes

--- WEB-DEV19/reactrouter/src/App.js
+++ WEB-DEV19/reactrouter/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Link,  Switch } from "react-router-dom";
 import {Users} from "./Users"
+import {About} from "./About"
+
 
 const Home = (props) => <h2>Home Page</h2>;
-const About = () => <h2>About Page</h2>;
+
 
 
 function App() {
   return (
     <BrowserRouter>
+    <div>
    <nav>
       <ul>
         <li>
@@ -23,6 +26,7 @@ function App() {
       </ul>
 
     </nav>
+    </div>
     <Switch>
       <Route exact={true} path="/" component={Home} />
       <Route path="/About" component={About} />
